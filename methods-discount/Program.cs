8dbad830afa959435715e6e32d814b30750d8fa7
nedakenanova <nedakenanova@abv.bg>
2025-07-08 15:04:19 +0300

    Create Program.cs
    Добавена задача: Изчисление на крайна цена с метод

---
+++ methods-discount/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Въведи цена:");
+double price = double.Parse(Console.ReadLine());
+Console.WriteLine("Въведи отстъпка %:");
+double discount = double.Parse(Console.ReadLine());
+Console.WriteLine($"Крайна цена:{GetDiscountPrice(price,discount):f2} ");
+Console.WriteLine($"Спестяваш:{price - GetDiscountPrice(price, discount):f2}");
+ double GetDiscountPrice(double price,double discount)
+{
+    double percent = discount / 100;
+    return price-(price*percent);
+}
